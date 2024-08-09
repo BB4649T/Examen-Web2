c2637c9be5a7d94bf4c9eb8904cffcaac5349765
BB4649T <you@example.com>
2024-08-09 23:09:34 +0300

    fix : resoud des bug dans styles.css et POssessionPage.js

--- src/components/PossessionsPage.js
+++ src/components/PossessionsPage.js
@@ -1,67 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Table, Form, Button, Container } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "../styles.css"; // Assure-toi que le chemin est correct
 import Possession from "../models/Possession"; // Assure-toi que le chemin est correct
 
 function PossessionsPage() {
-  const [people, setPeople] = useState([]);
   const [selectedPerson, setSelectedPerson] = useState("");
-  const [possessions, setPossessions] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [totalValue, setTotalValue] = useState(0);
-
+  const [people, setPeople] = useState([]);
   const [libelle, setLibelle] = useState("");
   const [valeurInitiale, setValeurInitiale] = useState("");
   const [dateDebut, setDateDebut] = useState("");
   const [dateFin, setDateFin] = useState("");
   const [amortissement, setAmortissement] = useState("");
-
-  useEffect(() => {
-    fetch("/src/data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setPossessions(data);
-      });
-  }, []);
+  const [possessions, setPossessions] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [totalValue, setTotalValue] = useState(0);
 
   const addPerson = (name) => {
-    if (name && !people.includes(name)) {
-      setPeople([...people, name]);
-      setSelectedPerson(name);
-    }
+    setPeople([...people, name]);
   };
 
   const addPossession = () => {
-    if (selectedPerson) {
-      setPossessions([
-        ...possessions,
-        {
-          libelle,
-          valeurInitiale: parseFloat(valeurInitiale),
-          dateDebut: new Date(dateDebut),
-          dateFin: new Date(dateFin),
-          amortissement: parseFloat(amortissement),
-          person: selectedPerson,
-        },
-      ]);
-
-      // Réinitialisation des champs
-      setLibelle("");
-      setValeurInitiale("");
-      setDateDebut("");
-      setDateFin("");
-      setAmortissement("");
-    }
+    const newPossession = new Possession(
+      libelle,
+      valeurInitiale,
+      dateDebut,
+      dateFin,
+      amortissement
+    );
+    setPossessions([
+      ...possessions,
+      { ...newPossession, person: selectedPerson },
+    ]);
+    setLibelle("");
+    setValeurInitiale("");
+    setDateDebut("");
+    setDateFin("");
+    setAmortissement("");
   };
 
   const calculateTotalValue = () => {
-    if (selectedPerson) {
-      const personPossessions = possessions.filter(
-        (possession) => possession.person === selectedPerson
-      );
-      const value = personPossessions.reduce(
-        (acc, possession) =>
+    const total = possessions
+      .filter((possession) => possession.person === selectedPerson)
+      .reduce((acc, possession) => {
+        return (
           acc +
           new Possession(
             possession.libelle,
@@ -69,11 +52,11 @@ function PossessionsPage() {
             possession.dateDebut,
             possession.dateFin,
             possession.amortissement
-          ).getValeurActuelle(selectedDate),
-        0
-      );
-      setTotalValue(value);
-    }
+          ).getValeurActuelle(selectedDate)
+        );
+      }, 0);
+
+    setTotalValue(total);
   };
 
   return (
@@ -81,16 +64,19 @@ function PossessionsPage() {
       <h1>Gestion des Possessions</h1>
 
       <Form className="mb-4">
-        <Form.Group controlId="formAddPerson">
-          <Form.Label>Ajouter une Personne</Form.Label>
+        <Form.Group controlId="formAddPerson" className="form-group-custom">
+          <Form.Label className="form-label-custom">
+            Ajouter une Personne
+          </Form.Label>
           <Form.Control
             type="text"
             placeholder="Nom de la personne"
             id="newPerson"
+            className="form-control-custom"
           />
           <Button
             variant="primary"
-            className="mt-2"
+            className="button-custom"
             onClick={() => {
               const personName = document.getElementById("newPerson").value;
               addPerson(personName);
@@ -103,12 +89,15 @@ function PossessionsPage() {
       </Form>
 
       <Form className="mb-4">
-        <Form.Group controlId="formSelectPerson">
-          <Form.Label>Sélectionner une personne</Form.Label>
+        <Form.Group controlId="formSelectPerson" className="form-group-custom">
+          <Form.Label className="form-label-custom">
+            Sélectionner une personne
+          </Form.Label>
           <Form.Control
             as="select"
             value={selectedPerson}
             onChange={(e) => setSelectedPerson(e.target.value)}
+            className="form-control-custom"
           >
             <option value="">Sélectionnez une personne</option>
             {people.map((person) => (
@@ -121,42 +110,61 @@ function PossessionsPage() {
       </Form>
 
       <Form className="mb-4">
-        <Form.Label>Ajouter une Possession</Form.Label>
-        <Form.Group>
+        <Form.Label className="form-label-custom">
+          Ajouter une Possession
+        </Form.Label>
+        <Form.Group className="form-group-custom">
           <Form.Control
             type="text"
             placeholder="Libellé"
             value={libelle}
             onChange={(e) => setLibelle(e.target.value)}
+            className="form-control-custom"
           />
+        </Form.Group>
+        <Form.Group className="form-group-custom">
           <Form.Control
             type="number"
             placeholder="Valeur Initiale"
             value={valeurInitiale}
             onChange={(e) => setValeurInitiale(e.target.value)}
+            className="form-control-custom"
           />
+        </Form.Group>
+        <Form.Group className="form-group-custom">
           <Form.Control
             type="date"
             placeholder="Date Début"
             value={dateDebut}
             onChange={(e) => setDateDebut(e.target.value)}
+            className="form-control-custom"
           />
+        </Form.Group>
+        <Form.Group className="form-group-custom">
           <Form.Control
             type="date"
             placeholder="Date Fin"
             value={dateFin}
             onChange={(e) => setDateFin(e.target.value)}
+            className="form-control-custom"
           />
+        </Form.Group>
+        <Form.Group className="form-group-custom">
           <Form.Control
             type="number"
             placeholder="Amortissement (%)"
             value={amortissement}
             onChange={(e) => setAmortissement(e.target.value)}
+            className="form-control-custom"
           />
-          <Button variant="primary" className="mt-2" onClick={addPossession}>
-            Ajouter Possession
-          </Button>
         </Form.Group>
+        <Button
+          variant="primary"
+          className="button-custom"
+          onClick={addPossession}
+        >
+          Ajouter Possession
+        </Button>
       </Form>
 
       <Table striped bordered hover>
